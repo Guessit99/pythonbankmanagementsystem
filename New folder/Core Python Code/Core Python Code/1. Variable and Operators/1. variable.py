ed998cf70e5e727165c0e8761662e4a2b2e7741f
Azhar Shah <azharshah1613@gmail.com>
2023-04-29 23:47:27 +0530

    first commit

---
+++ New folder/Core Python Code/Core Python Code/1. Variable and Operators/1. variable.py
@@ -0,0 +1,25 @@
+# Declaring and initializing Variable
+# Individual
+a = 25
+b = 60
+c = "FirstName"
+d = 'LastName'
+
+# Multiple
+e, f, g, h = 25, 60, "FirstName", 'LastName'
+
+# Multiple with same value
+i = j = k = l = m = n = True
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(n)
+print("$"*20)
---
+++ New folder/Core Python Code/Core Python Code/1. Variable and Operators/2. arithmeticOperator.py
@@ -0,0 +1,56 @@
+# Addition
+# print(4+2)
+a = 25
+b = 15.20
+total = a + b
+print(total)
+
+# Subtraction
+# print(8-2)
+a = 25
+b = 15.20
+total = a - b
+print(total)
+
+# Multiplication
+# print(4*2)
+a = 25
+b = 25
+value = a * b
+print(value)
+
+# Divison
+# print(4/2)
+a = 60
+b = 15
+value = a / b
+print(value)
+print(type(value))
+
+# Modulus
+# print(5%2)
+a = 5
+b = 2
+value = a % b
+print(value)
+
+# Exponent
+# print(5**2)
+a = 5
+b = 2
+value = a**b
+print(value)
+
+# Floor Division Positive
+# print(5//2)
+a = 5
+b = 2
+value = a//b
+print(value)
+
+# Floor Division Negative
+# print(-5//2)
+a = -5
+b = 2
+value = a//b
+print(value)
